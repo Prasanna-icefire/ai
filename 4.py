e70225cbeff50399826dcfd2cb6b95c63d8757ea
Prasanna-icefire <prasannamg3@gmail.com>
2021-02-28 14:18:37 +0530

    shortened minimax

--- 4.py
+++ 4.py
@@ -1,31 +1,25 @@
-#minimax
 import math
-import random 
-#minimax class
-def minimax (currentDepth, nodeIndex, 
-             maxTurn, score,  
-             tarDepth): 
-  
-    # base case : tarDepth reached 
-    if (currentDepth == tarDepth):  
-        return score[nodeIndex] 
-      
-    if (maxTurn): 
-        return max(minimax(currentDepth + 1, nodeIndex * 2,  
-                    False, score, tarDepth),  
-                   minimax(currentDepth + 1, nodeIndex * 2 + 1,  
-                    False, score, tarDepth)) 
-      
-    else: 
-        return min(minimax(currentDepth + 1, nodeIndex * 2,  
-                     True, score, tarDepth),  
-                   minimax(currentDepth + 1, nodeIndex * 2 + 1,  
-                     True, score, tarDepth)) 
-      
-# Driver code 
-score = random.sample(range(1, 50), 4)
-print(str(score))  
-treeDepth = math.log(len(score), 2) 
-  
-print("The optimal value is : ", end = "") 
-print(minimax(0, 0, True, score, treeDepth)) 
+
+
+def minimax(tree, depth):
+    max_turn = bool(depth % 2)
+
+    for _ in range(depth):
+        zipped = zip(tree[::2], tree[1::2])
+        print(tree[::2], tree[1::2])
+
+        if max_turn:
+            tree = [max(a, b) for a, b in zipped]
+            # print(tree)
+        else:
+            tree = [min(a, b) for a, b in zipped]
+            # print(tree)
+        max_turn = not max_turn
+
+    return tree[0]
+
+
+A = [3, 5, 2, 9, 12, 5, 23, 23]
+depth = math.ceil(math.log(len(A), 3))
+print(minimax(A, depth))
+
